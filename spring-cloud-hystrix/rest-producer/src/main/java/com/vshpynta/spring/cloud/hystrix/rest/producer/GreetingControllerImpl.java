856f3d38130d33e8667926617716171707c85707
Volodymyr Shpynta <vshpynta@playtika.com>
2019-04-18 18:35:40 +0300

    Adjusted rest producer to work with Eureka server

--- spring-cloud-hystrix/rest-producer/src/main/java/com/vshpynta/spring/cloud/hystrix/rest/producer/GreetingControllerImpl.java
+++ spring-cloud-hystrix/rest-producer/src/main/java/com/vshpynta/spring/cloud/hystrix/rest/producer/GreetingControllerImpl.java
@@ -1,18 +1,31 @@
 package com.vshpynta.spring.cloud.hystrix.rest.producer;
 
+import com.netflix.discovery.EurekaClient;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import static java.lang.String.format;
 
 @RestController
 @Slf4j
 public class GreetingControllerImpl implements GreetingController {
 
+    @Autowired
+    @Lazy
+    private EurekaClient eurekaClient;
+
+    @Value("${spring.application.name}")
+    private String appName;
+
     @Override
     public String greeting(@PathVariable("username") String username) {
         simulateLongTermOperation();
-        return String.format("Hello %s!\n", username);
+        return format("Hello %s, from %s!", username, eurekaClient.getApplication(appName).getName());
     }
 
     @SneakyThrows
--- spring-cloud-hystrix/rest-producer/src/main/java/com/vshpynta/spring/cloud/hystrix/rest/producer/RestProducerApplication.java
+++ spring-cloud-hystrix/rest-producer/src/main/java/com/vshpynta/spring/cloud/hystrix/rest/producer/RestProducerApplication.java
@@ -2,8 +2,10 @@ package com.vshpynta.spring.cloud.hystrix.rest.producer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient //this annotation is optional if we have the spring-cloud-starter-netflix-eureka-client dependency on the classpath.
 public class RestProducerApplication {
 
     public static void main(String[] args) {
