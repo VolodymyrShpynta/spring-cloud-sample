737754028edaadf9a52943085ee7796a404921ab
Volodymyr Shpynta <vshpynta@playtika.com>
2019-05-14 19:09:27 +0300

    Integrated Spring Cloud Config service with Eureka server

--- spring-cloud-config-server/src/main/java/com/vshpynta/spring/cloud/config/server/ConfigServer.java
+++ spring-cloud-config-server/src/main/java/com/vshpynta/spring/cloud/config/server/ConfigServer.java
@@ -1,11 +1,11 @@
 package com.vshpynta.spring.cloud.config.server;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@SpringBootApplication
 @EnableConfigServer
+@SpringCloudApplication
 public class ConfigServer {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServer.class, args);
--- spring-cloud-hystrix/feign-rest-consumer/src/main/java/com/vshpynta/spring/cloud/hystrix/rest/consumer/RestConsumerFeignApplication.java
+++ spring-cloud-hystrix/feign-rest-consumer/src/main/java/com/vshpynta/spring/cloud/hystrix/rest/consumer/RestConsumerFeignApplication.java
@@ -1,13 +1,11 @@
 package com.vshpynta.spring.cloud.hystrix.rest.consumer;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
-@EnableCircuitBreaker
+@SpringCloudApplication
 @EnableHystrixDashboard
 @EnableFeignClients
 public class RestConsumerFeignApplication {
